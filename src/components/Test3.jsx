d38122b1d90844df557d7fdb8ca1de1aaa3dac16
Zi90 <edwin0322@gmail.com>
2024-09-04 12:37:55 +0900

    day01

---
+++ src/components/Test3.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test3 = () => {
+    return (
+        <div className='test3'>
+            <div>Test3.jx의 영역입니다.</div>
+        </div>
+    );
+};
+
+export default Test3;
